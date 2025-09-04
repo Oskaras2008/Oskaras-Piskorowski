e787e5d133505af82715566bedb9a834e21e77a1
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-04 09:24:16 +0100

    Add files via upload

---
+++ Programme 11 Sept.py
@@ -0,0 +1,9 @@
+#Author Oskaras Piskorowski
+#Date 4th September
+#Programme for user to enter a number less than 20 . if the number is more, print a message that says 'too high'
+
+number = int(input('Enter a number that is 20 or less: '))
+if (number>20):
+    print('Too High')
+else:
+    print('Thank you')
