bcf30684ebe378643a6894e90e4cdc273c88766f
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-09 14:35:20 +0100

    Add files via upload

---
+++ Loops Sept 5.py
@@ -0,0 +1,8 @@
+#Author Oskaras Piskorowski
+#Date 9th September 2025
+#Programme that prints a multiplication table of given number
+
+number = int(input('Enter a number: '))
+
+for i in range (1,12):
+    print(i*number)
