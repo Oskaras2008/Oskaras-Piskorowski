cc329568b66cd49bfc2f369a98287d89b5d1f9fd
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-09 13:51:49 +0100

    Delete Assigment 1  directory

--- Assigment 1 /Programme 2 Sept.py
+++
@@ -1,8 +0,0 @@
-#Author Oskaras Piskorowski
-#Date 1st September 2025
-#Programme Ask user for their name and output Hello <User_Name>
-#Expand and ask for surname too
-
-user_name = input('Enter your name: ')
-user_surname = input('Enter your surname: ')
-print('Hello', user_name, user_surname)
