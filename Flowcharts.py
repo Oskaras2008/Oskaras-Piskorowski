fe4f2c3113c8dcf8870a427762fdb71a7f67a5c0
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:04:41 +0100

    Add files via upload

---
+++ Flowcharts.py
@@ -0,0 +1,13 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Find area and perimeter of rectangle
+
+l = float(input('Length of rectangle: '))
+w = float(input('Width of rectangle: '))
+
+p = l+l+w+w
+print('Perimeter of rectangle is' ,p)
+
+a = l*w
+print('Area of rectangle is',a)
+
