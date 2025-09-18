e604cfce63943a252566f518a3604debffdafce2
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:15:12 +0100

    Add files via upload

---
+++ Flowcharts 2.py
@@ -0,0 +1,10 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Calculate simple interest
+
+p = float(input('pricipal:'))
+r = float(input('rate: '))
+t = float(input('time: '))
+i = p*(r/10)*t
+
+print('simple interest is',i)
