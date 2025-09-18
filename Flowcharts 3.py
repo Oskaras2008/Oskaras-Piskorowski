e2e573f18e296d184623b35125e46f7c8499974c
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:18:34 +0100

    Add files via upload

---
+++ Flowcharts 3.py
@@ -0,0 +1,10 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Check if a year is a leap year
+
+y = int(input('What year is it? '))
+if y%4 == 0:
+    print('Leap Year')
+    
+else:
+    print('Not leap year')
