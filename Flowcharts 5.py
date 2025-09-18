77aac53915bcaf1ab91d56ced2ccbd4eaab5063d
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:24:34 +0100

    Add files via upload

---
+++ Flowcharts 5.py
@@ -0,0 +1,9 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Check if number is odd or even
+
+n = int(input('Enter a number: '))
+if n%2 ==0:
+    print('Even number')
+elif n%2 ==1:
+    print('Odd number')
