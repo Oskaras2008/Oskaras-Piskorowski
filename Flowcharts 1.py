e0d0591007edb9530cced9d774cbb7b126c036db
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:09:27 +0100

    Add files via upload

---
+++ Flowcharts 1.py
@@ -0,0 +1,9 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Calculate area and circumference of circle
+
+r = float(input('Radius of circle: '))
+a = 3.14*(r**2)
+c = 2*(3.14*(r**2))
+print('area of circle is:',a)
+print('circumference of circle is',c)
