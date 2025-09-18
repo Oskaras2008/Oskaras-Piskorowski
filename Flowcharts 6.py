793dbcefbf5838f0b566ef6d7d78dda9fc04dd30
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:30:07 +0100

    Add files via upload

---
+++ Flowcharts 6.py
@@ -0,0 +1,11 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Categorise person as a child, teenager or adult
+
+age=int(input('Enter your age: '))
+if age<13:
+    print('child')
+elif (age >=13) and (age <20):
+    print('teenager')
+else:
+    print('adult')
