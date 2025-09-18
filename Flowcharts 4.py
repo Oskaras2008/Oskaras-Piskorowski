651062de4c9516b4748082828833eabda7817770
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-18 09:22:16 +0100

    Add files via upload

---
+++ Flowcharts 4.py
@@ -0,0 +1,9 @@
+#Oskaras Piskorowski
+#18th of September 2025
+#Check if number is positive or negative
+
+n=int(input('Enter a number: '))
+if n>=0:
+    print('postive')
+elif n<=0:
+    print('negative')
