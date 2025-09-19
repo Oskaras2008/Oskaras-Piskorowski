b6d8feb8b95c03d93d168ee521ec28b0c4f78352
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-19 09:34:49 +0100

    Add files via upload

---
+++ Flowcharts 12.py
@@ -0,0 +1,14 @@
+#Oskaras Piskorowski
+#19th of September 2025
+#To print squares of first n numbers
+
+total = 0
+count = 0
+number = 0
+number = int(input('Enter a number: '))
+
+for i in range(1,number+1):
+    #number = float(input('Enter a number: '))
+    count +=1
+    
+    print(count**2)
