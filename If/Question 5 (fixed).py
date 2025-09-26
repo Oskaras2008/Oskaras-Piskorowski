77cf1e5e87c978ef787cc17915b4a891e8c13b38
mrdilleen <mrdilleen@protonmail.com>
2025-09-26 19:44:44 +0100

    Add files via upload

---
+++ If/Question 5 (fixed).py
@@ -0,0 +1,17 @@
+#Author Oskaras Piskorowski
+#Date 4th September
+#Programme that asks user if it is raining if they enter yes ask if its windy if they say yes to that
+#then say that it is too windy for an umbrella otherwise display the message take an umbrella and if they said
+#no to the first question then say enjoy your day
+
+rainy = input('Is it raining? ')
+
+if rainy == ('yes') or rainy == ('Yes'):
+        windy = input('is it windy? ')
+        if windy == ('yes') or windy == ('Yes'):
+            print('It is too windy for an umbrella')         
+        else:
+            print(' Bring an umbrella')
+
+else:
+        print('enjoy your day')
