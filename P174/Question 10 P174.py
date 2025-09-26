cbbfbcfc0f3db65ddf6487c08115fd001bd79200
mrdilleen <mrdilleen@protonmail.com>
2025-09-26 19:50:51 +0100

    Add files via upload

---
+++ P174/Question 10 P174.py
@@ -0,0 +1,11 @@
+#Author Oskaras Piskorowski
+#Date 2nd September 2025
+#Programme to fin are of triangle
+
+length = int(input('Give a length of a triangle: '))
+height = int(input('Give the height of the triangle: '))
+
+area = length*height //2
+area = int(area)
+
+print('The area of the triangle is',area )
---
+++ P174/Question 6 P174.py
@@ -0,0 +1,11 @@
+#Author Oskaras Piskorowski
+#Date 2nd September 2025
+#Programme Write a Python programme that accepts the radius of a circle and prints its area
+
+radius = input('What is the radius of your circle in cm? ')
+radius = int(radius)
+
+area = ((radius**2)*3.14159)
+area = int(area)
+
+print('The area of your circle is',area,'cm squared')
