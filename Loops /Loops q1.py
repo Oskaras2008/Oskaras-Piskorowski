ea1e4ddcc5b8b928a398971b1bab42a496ce3eb4
mrdilleen <mrdilleen@protonmail.com>
2025-09-26 19:48:02 +0100

    Delete Loops  directory

--- Loops /Loops q1.py
+++
@@ -1,8 +0,0 @@
-#Author Oskaras Piskorowski
-#Date 8th September 2025
-#Programme that asks use for name and prints it out 3 times
-
-name = input('Enter your name: ')
-
-for word in range(3):
-    print(name)
