52c074e529af14783b952845f9ee26fff685d0bf
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-09-30 13:49:49 +0100

    Update Loops Sept 2.py

--- Loops/Loops Sept 2.py
+++ Loops/Loops Sept 2.py
@@ -2,9 +2,10 @@
 #Date 9th September 2025
 #Programme that prints all even numbers from 1 to given amount
 
-
-for i in range(1,11):
+number = int(input('Enter a number: '))
+for i in range(1,number):
     if i%2 == 0:
         print(i)
     else:
         pass
+
