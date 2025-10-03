7c8e57ba389f4540dddfcb2d29205ccc6e368da7
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-03 08:58:47 +0100

    Add files via upload

---
+++ While 3.py
@@ -0,0 +1,11 @@
+#Oskaras Piskorowski
+#3rd October 2025
+#Program that takes an integer number and prints all even numbers
+
+n = int(input('enter a number: '))
+
+while n>0:
+    for i in range(0,n+1,2):
+        print(i)
+    else:
+        break
