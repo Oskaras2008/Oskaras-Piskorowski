5325a7a13b997ae4e9212dc90762de58ea04811f
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-06 12:30:57 +0100

    Update While 8.py

--- Assignment 6 /While 8.py
+++ Assignment 6 /While 8.py
@@ -1,13 +1,15 @@
 #Oskaras Piskorowski
 #6rd October 2025
-#Program that asks user to guess the value, if it is low, print too low, if it is too high, print too high
+#Program that asks user to guess the value, if it is low, print too low, if it is too high, print too high, print how much attempts it took
 
 n=167
+a=0
 while n==167:
     guess=int(input('Guess my number: '))
+    a+=1
     if guess>n:
         print('Too high')
     elif guess<n:
         print('Too low')
     else:
-        print('Correct')
+        print('Correct', 'it took',a,'attempts')
