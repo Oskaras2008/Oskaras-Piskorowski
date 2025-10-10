4bacf3421946bc31a315dcf65718868bb5c7454d
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-10 09:34:14 +0100

    Add files via upload

---
+++ Assignment 8 4.py
@@ -0,0 +1,21 @@
+#Oskaras Piskorowski
+#10th October 2025
+#Write a program to input N numbers and then print the second largest number.
+
+maximum=0
+secondmax=maximum
+count=0
+c=67
+number=int(input('How many numbers will you enter'))
+while c==67:
+    n=int(input('Enter a number: '))
+    if maximum<n:
+        secondmax=maximum
+        maximum=n
+    else:
+        secondmax=n
+    count+=1
+    if count==number:
+        break
+print(secondmax)
+    
