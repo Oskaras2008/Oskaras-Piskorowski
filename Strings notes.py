3ac0e4d597f42dedeb358c404930c604f1f10ffb
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-16 09:29:32 +0100

    Add files via upload

---
+++ Strings notes.py
@@ -0,0 +1,80 @@
+'''variable1= 'This string'\
+' goes over'\
+' many'\
+' lines'
+print(variable1)'''
+
+#backslash prints the strings on the same line
+#or you can enclose it in brackets
+
+'''variable1= ('This string'
+' goes over'
+' many'
+' lines')
+print(variable1)'''
+
+#multiline string
+'''variable1= """This string
+ goes over
+ many
+ lines"""
+print(variable1)'''
+
+#String addition concatenation
+'''fruit = 'ap'+'ple'
+print(fruit)'''
+
+#String multiplication(repetition)
+'''random_word = 'He'
+print(random_word*3)'''
+
+#casting strings
+#convert strings to numbers
+
+'''variable1 = '12'
+variable2 = '12.3'
+
+print(int(variable1))
+print(float(variable2))'''
+
+#You can convert numbers into strings by using str()
+'''variable3 = 12
+variable4 = 12.3
+print(str(variable3))
+print(str(variable4))'''
+
+variable5 = 'Hello World!'
+#prints the letter 'e' with index positioning 1
+'''print(variable5[1])
+#Prints the letter 'e ' with index position -11
+print(variable5[-11])'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
