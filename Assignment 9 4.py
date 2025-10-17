fe13fa2b591e8d17a14deaac74e539d71c9094d3
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-17 19:09:33 +0100

    Add files via upload

---
+++ Assignment 9 4.py
@@ -0,0 +1,10 @@
+#Oskaras Piskorowski
+#17th of October 2025
+#Assignment 9 q 4
+#Q4. Write a program to count the number of times a character occurs in the given string.
+#The string and the character should be input by the user
+
+word=str(input('Enter a sentence: '))
+ca=str(input('enter a charachter that is in your sentence: '))
+
+print(word.count(ca))
