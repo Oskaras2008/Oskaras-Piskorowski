e61e32365c8280e60423dd306328b69341787f35
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-21 14:21:55 +0100

    Add files via upload

---
+++ Assignment  10 3.py
@@ -0,0 +1,14 @@
+#Oskaras Piskorowski
+#21st of October 2025
+
+#ii) A while loop
+
+word= input('Enter a word: ')
+c=1
+count=0
+while count<len(word):
+    count+=1
+
+print(count)
+
+    
---
+++ Assignment 10 2.py
@@ -0,0 +1,13 @@
+#Oskaras Piskorowski
+#21st of October 2025
+#Write a program to calculate the length of a string using :
+#i) A for loop
+
+
+string=input('Enter a string: ')
+count=0
+
+for i in string:
+    count+=1
+    
+print(count)
