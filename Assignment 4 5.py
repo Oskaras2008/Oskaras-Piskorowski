fe7e7c4b46097a3bf86fba60fa1b72be80983c4d
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-10-21 14:35:51 +0100

    Add files via upload

---
+++ Assignment 4 5.py
@@ -0,0 +1,6 @@
+#Oskaras Piskorowski
+#21st of October 2025
+#Write a Python program to remove characters that have odd index values in a given
+#string.
+word=input('Enter a word: ')
+print(word[0::2])
