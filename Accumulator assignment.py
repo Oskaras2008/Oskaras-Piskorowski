1591dc9dba6863c6ac361d2844e836d63df46dd5
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2025-12-08 12:42:41 +0000

    Add files via upload

---
+++ Accumulator assignment.py
@@ -0,0 +1,63 @@
+# Oskaras Piskorowski
+#Accumulator Assignment
+#12-8-2025
+
+count = 0
+for i in range(1,51):
+    count+=i
+    
+print(count)
+
+count1 = 0
+n = int(input('Enter n: '))
+for i in range (1, n +1):
+    count1+=i
+    
+print(count1)
+
+product = 1
+for i in range(1,6):
+    product=product*i
+    
+print(product)
+
+count2 = 0
+for i in range(2,51,2):
+    count2+=i
+    
+print(count2)
+
+count3 = 0
+n1 = int(input('Enter n: '))
+
+for i in range(1,n1+1,2):
+    count3+=i
+         
+print(count3)
+
+count4 = 0
+
+for i in range(0,51,3):
+    count4= count4+i
+    
+print(count4)
+
+
+count_5 = 1
+for i in range(2,51,2):
+    count_5 = count_5*i
+print(count_5)
+ 
+count6 = 1
+for i in range(5,51,5):
+    count6 = count6*i
+print(count6)
+
+count7 = 0
+for i in range(1,11):
+    count7+=(i*2)
+print(count7)
+
+
+
+
