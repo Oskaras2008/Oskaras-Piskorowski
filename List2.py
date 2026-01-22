2da91ade3a05bc1dc0e4a654bee2bb27507b2fa0
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2026-01-22 08:57:18 +0000

    Add files via upload

---
+++ List2.py
@@ -0,0 +1,10 @@
+#Oskaras Piskorowski
+#19/1/26
+#List assignment
+#q2
+
+
+list1 = eval(input('Enter a list: '))
+list1.reverse()
+print(list1)
+
---
+++ List3.py
@@ -0,0 +1,11 @@
+#Oskaras Piskorowski
+#19/1/26
+#List assignment
+
+#q3
+
+list1 = eval(input('Enter a list: '))
+list2 = eval(input('Enter another list: '))
+print(list1 + list2)
+
+    
