d400758cc6614e2c0498362e390c028d9ca67311
Oskaras2008 <21opiskorowsk.acc@lwetb.ie>
2026-01-22 09:33:08 +0000

    Add files via upload

---
+++ List7.py
@@ -0,0 +1,20 @@
+#Oskaras Piskorowski
+#22/1/26
+#List assignment
+
+#q7
+list=[]
+for i in range(0,50):
+    list.append(i)
+print(list)
+
+list1=[]
+for i in range(0,50):
+    list1.append(i**2)
+print(list1)
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+count=1
+for i in alpha:
+    print(i*count)
+    count+=1
